33d92d3de92d41d0e0ac4bf9003c2403c1fbd275
Nikesh Posam <nikesh.posam@gmail.com>
2025-11-27 17:18:49 +0530

    [TYPESCRIPT] Server moved to typescript

--- api/src/controllers/requestController.ts
+++ api/src/controllers/requestController.ts
@@ -1,4 +1,4 @@
-import type { NextFunction, Request, Response } from "express";
+import type { Request, Response } from "express";
 import { isEmpty } from "radash";
 type Resource = {
     id: number,
@@ -23,27 +23,27 @@ class RequestController {
         this.addRequest = this.addRequest.bind(this);
         this.updateRequest = this.updateRequest.bind(this);
     }
-    getRequests(req: Request<unknown, unknown, unknown, Pick<Resource, "status">>, res: Response<Array<Resource>, unknown>) {
+    getRequests(req: Request<unknown, unknown, unknown, Pick<Resource, "status">>, res: Response<Array<Resource>>) {
         const { query: filters } = req;
         const status = isEmpty(filters.status) ? ["PENDING", "APPROVED", "REJECTED"] : filters.status.split(",");
         const result = this.requests.filter(r => status.includes(r.status))
-        res.status(200).send(result.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime()));
+        return res.status(200).send(result.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime()));
     }
-    addRequest(req: Request<unknown, unknown, Resource, unknown>, res: Response<Resource | ServerError, unknown>) {
+    addRequest(req: Request<unknown, unknown, Resource, unknown>, res: Response<Resource | ServerError>) {
         const { body: payload } = req;
         if (!payload) {
-            res.status(404);
+            return res.status(404);
         }
         const existingEmail = this.requests.filter(r => payload.email == r.email && r.status == "PENDING")
         if (existingEmail.length > 3) {
-            res.status(412).send({
+            return res.status(412).send({
                 err: "Bulk Request",
                 message: "Total number of request exceeded"
             });
         }
         const existRequest = existingEmail.find(r => payload.resource == r.resource)
         if (existRequest) {
-            res.status(412).send({
+            return res.status(412).send({
                 err: "Duplicate Request",
                 message: "Request alreayd exists!"
             });
@@ -56,20 +56,20 @@ class RequestController {
             createdAt: new Date(),
         }
         this.requests.push(request);
-        res.status(201).send(request);
+        return res.status(201).send(request);
     }
-    updateRequest(req: Request<{ id: number }, unknown, Partial<Resource>, unknown>, res: Response<Resource | ServerError, unknown>) {
+    updateRequest(req: Request<{ id: number }, unknown, Pick<Resource, "status">, unknown>, res: Response<Resource | ServerError>) {
         const { params: { id }, body: payload } = req;
-        const request = this.requests.find(r => r.id == id && r.status == "PENDING")
+        const request = this.requests.find(r => r.id == id && r.status == "PENDING");
         if (!request) {
-            res.status(404).send({
+            return res.status(404).send({
                 err: "Not Found",
                 message: "Id doesn't exisits"
             })
         }
         request.status = payload.status;
         request.updatedAt = new Date();
-        res.status(200).send(request);
+        return res.status(200).send(request);
     }
 }
 
