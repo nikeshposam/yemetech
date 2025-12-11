c82d6171ccd1ed7045511bffa3b89203574bd3d0
Nikesh Posam <nikesh.posam@gmail.com>
2025-12-12 05:27:15 +0530

    [YEMETECH] Minor refactoring

--- api/src/server.ts
+++ api/src/server.ts
@@ -7,5 +7,11 @@ const server = express();
 server.use(express.json());
 server.use(cors({ origin: "*" }));
 server.use("/api/requests", requestsRoute);
+server.get("/api/healthz", (req, res) => {
+    return res.send("YES! I'M ALIVE");
+});
+server.get("/api/", (req, res) => {
+    return res.send("OKAY!!");
+});
 
 export default server;
